cea8a4ddf07f2371c551074050ceb5c8f5d3fbc9
KofiFloki <Jeremiah.martey@yahoo.com>
2023-10-17 16:07:50 -0700

    Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,2 +1,12 @@
-// Lists all documents with name starting by Holberton in the school collection.
-db.school.find({name: /Holberton.*/}).forEach(printjson);
+#!/usr/bin/env python3
+'''Task 10's module.
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of a collection's document based on the name.
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
